f133af5b4ea59ea4860f13350bab878f04009c66
paudel54 <2sansrit@gmail.com>
2023-06-25 17:04:50 +0545

    Created controller backend logic for finance app

--- server/controllers/income.js
+++ server/controllers/income.js
@@ -25,6 +25,29 @@ exports.addIncome = async (req, res) => {
     } catch (error) {
         res.status(500).json({ message: 'Server Error' })
     }
-
     console.log(income)
+}
+
+//controller to get Items
+exports.getIncomes = async (req, res) => {
+    try {
+        // soritng the items so last item would be on top
+        const incomes = await IncomeSchema.find().sort({ createdAt: -1 })
+        res.status(200).json(incomes)
+    } catch (error) {
+        res.status(500).json({ message: 'Server Error' })
+    }
+}
+
+
+// Controllers to delete Income datas. 
+exports.deleteIncome = async (req, res) => {
+    const { id } = req.params;
+    IncomeSchema.findByIdAndDelete(id)
+        .then((income) => {
+            res.status(200).json({ message: 'Income Deleted' })
+        })
+        .catch((err) => {
+            res.status(500).json({ message: 'Server Error' })
+        })
 }
