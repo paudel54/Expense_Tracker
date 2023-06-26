be73071651fa9360e374514770934a1c80ff4b48
paudel54 <2sansrit@gmail.com>
2023-06-26 07:49:34 +0545

    Created CRD backend API endpoints

--- server/routes/transactions.js
+++ server/routes/transactions.js
@@ -1,4 +1,5 @@
 const router = require('express').Router()
+const { addExpense, getExpense, deleteExpense } = require('../controllers/expense')
 const { addIncome, getIncomes, deleteIncome } = require('../controllers/income')
 router.get('/', (req, res) => {
     res.send('testing router path ')
@@ -8,5 +9,8 @@ router.post('/add-income', addIncome)
     .get('/get-incomes', getIncomes)
     //:Id can be anything like string or Number or any dyanmic value
     .delete('/delete-income/:id', deleteIncome)
+    .post('/add-expense', addExpense)
+    .get('/get-expenses', getExpense)
+    .delete('/delete-expense/:id', deleteExpense)
 
 module.exports = router
